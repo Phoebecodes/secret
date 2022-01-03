990fdeed88b176a2d9b5a129c72978271eda3786
Phoebe <phoebe@PhoebedeMacBook-Air.local>
2022-01-03 14:19:45 +0800

    use hash

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //jshint esversion:6
-require ("dotenv"),config();
+// require ("dotenv"),config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
 const mongoose = require ("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require ("md5");
  
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User = mongoose.model("User",userSchema);
 
@@ -43,7 +43,7 @@ app.listen(3000, function(){
 
 app.post("/register",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     const newUser = new User ({
         username : username,
@@ -61,7 +61,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({username: username},function(err,foundUser){
         if (err){
